fbf5544b185d7c1e9d2c287e4316df5fa13408f9
Gilvan Silva <gpsilva@gmail.com>
2021-02-11 16:06:46 -0300

    first commit

---
+++ src/test/java/br/sp/gpsilva/tasks/api/test/APITest.java
@@ -0,0 +1,51 @@
+package br.sp.gpsilva.tasks.api.test;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+		;
+	}
+	
+	@Test
+	public void deveAdicionarTarefasComSucesso() {
+		RestAssured.given()
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2021-02-11\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void naodeveAdicionarTarefaInvalida() {
+		RestAssured.given()
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2011-02-11\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(400)
+			.body("message",CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+}
